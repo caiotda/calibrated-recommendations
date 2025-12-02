5a9a8d4cef9ed9e9b5f7fe7d33a2225fd3a48dc2
Caio Andrade <caiotdandrade@gmail.com>
2025-12-02 18:16:54 -0300

    Simplifies MACE calculation (#1)
    * Fixes NaN bug
    
    * Simplifies calibration error calculation

--- metrics.py
+++ metrics.py
@@ -2,11 +2,10 @@ import numpy as np
 from scipy.stats import entropy
 from tqdm import tqdm
 
-from calibratedRecs.constants import USER_COL, ITEM_COL, GENRE_COL
-from calibratedRecs.calibrationUtils import calculate_genre_distribution, element_wise_sub_module, count_zero_in_both
+from calibratedRecs.constants import USER_COL
+from calibratedRecs.calibrationUtils import calculate_genre_distribution, element_wise_sub_module
 
 from calibratedRecs.distributions import standardize_prob_distributions
-
 
 def KL(p, q):
     return entropy(p, q)
@@ -39,22 +38,15 @@ def CE_at_k(rec_list, user_history, item2genreMap, k=20):
     # E se o tipo de calibração muda? A maneira de calcular a distribuição deveria mudar também.
     rec_dist = calculate_genre_distribution(rec_at_k, item2genreMap)
     user_history_dist = calculate_genre_distribution(user_history, item2genreMap)
-
-    # by default, we consider every genre possible in the distribution. Because MACE is an average
-    # of differences between p(g|u) and q(g|u), if we consider samples where genres -therefore p(g|u)
-    # and q(g|u) = 0 - we would be underestimating the calibration error as the 0 differences would
-    # Diminish the average calibration error observed.
-    user_history_dist_filtered = {
-        k: v for k, v in user_history_dist.items()
-        if v > 0 or rec_dist.get(k, 0) > 0
-    }
-    rec_dist_filtered = {k:v for k, v in rec_dist.items() if v > 0 or user_history_dist.get(k, 0) > 0}
-
     
     # Checks the absolute difference for every genre pertaining to an item in the recommendation
     # list or in the user history
-    distribution_shift = element_wise_sub_module(user_history_dist_filtered, rec_dist_filtered) 
-    return np.mean(list(distribution_shift.values()))
+
+
+    user_history_std, rec_dist_std = standardize_prob_distributions(user_history_dist, rec_dist)
+    distribution_shift = list(element_wise_sub_module(user_history_std, rec_dist_std).values())
+    
+    return np.mean(distribution_shift).item()
 
 def ace(rec_list, user_history, item2genreMap):
     N = len(rec_list)
@@ -75,9 +67,7 @@ def mace(df, user2history, recCol, item2genreMap, subset=None):
         ACE_U = 0
         for u in tqdm(df.index, total=num_users):
             row = df.iloc[u]
-            u = row["user"]
             rec = row[recCol]
             history = user2history[u]
             ACE_U += ace(rec, history, item2genreMap)
-
         return ACE_U / num_users
