f1d6d62e38bbeb5cc628b9c4fbefc7143b70fee0
Caio Andrade <caiotdandrade@gmail.com>
2025-12-05 10:29:46 -0300

    Moves utils code to apropriate file

--- metrics.py
+++ metrics.py
@@ -37,20 +37,6 @@ def get_kl_divergence(
     q_normalized = q_clipped / q_clipped.sum()
     return KL(p_normalized, q_normalized).item()
 
-def build_weight_tensor(user_tensor, rec_ids, rec_scores, n_items, k):
-        rec_at_k = rec_ids[:, :k]
-        rec_ids_index = rec_at_k.reshape(1, -1)
-
-        scores_at_k = rec_scores[:, :k]
-        scores_index = scores_at_k.reshape(1, -1)
-
-        user_tensor_interleaved = user_tensor.repeat_interleave(k)
-        
-        n_users = len(user_tensor)
-        w_u_i = torch.zeros(size=(n_users, n_items))
-        w_u_i[user_tensor_interleaved, rec_ids_index] = scores_index
-
-        return w_u_i.to(torch.float64)
 
 
 def CE(weight_tensor, user_history_tensor, p_g_i):
