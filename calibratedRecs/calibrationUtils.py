Documents code, Improves performance and fixes bug on kl div (#5)
* Changes how dist clipping works and removes nan to num set

* Explicitly removes candidate by idx

* Avoids recomputing distribution tensor for every candidate. Does it iteratively

* Removes unused code

calibratedRecs/calibrationUtils.py
@@ -14,12 +14,33 @@ def preprocess_dataframe_for_calibration(df):
     processed_df = df.copy()
     processed_df[GENRE_COL] = processed_df[GENRE_COL].apply(tuple)
     processed_df["constant"] = 1
+    # We shift scores to be strictly positive per user in order to
+    # avoid zero denominators in the KL divergence calculation
+    processed_df["rating"] = processed_df.groupby("user")["rating"].transform(
+        lambda x: (x - x.min()) + 1e-8
+    )
     # processed_df = get_linear_time_weight_rating(processed_df)
     return processed_df
 
 
-def build_item_genre_distribution_tensor(df, n_items, distribution_mode="steck"):
+def build_item_genre_distribution_tensor(df, distribution_mode="steck"):
+    """
+        Builds genre distribution tensor for each item (p_g_i, as in Steck's paper).
+        Parameters
+    ----------
+        df : pandas.DataFrame
+            DataFrame containing at least the columns named by the global constants ITEM_COL and GENRE_COL
+        distribution_mode : str, optional
+            Mode for calculating genre distribution.
+            Currently only "steck" is supported, which creates a distribution based on the frequency of genres for each item. Default is "steck".
+        Returns
+        -------
+        p_g_i : torch.FloatTensor
+            Tensor of shape (n_items, n_genres) where each row represents the genre distribution
+            for the corresponding item. The tensor is on the device referenced by the global variable `dev`.
+    """
     item2genre = df[[ITEM_COL, GENRE_COL]].drop_duplicates()
+    n_items = item2genre.item.max() + 1
     all_genres = item2genre.explode("genres")["genres"].drop_duplicates().tolist()
     n_genres = len(all_genres)
     std_dict = {genre: 0 for genre in all_genres}
@@ -40,6 +61,27 @@ def build_item_genre_distribution_tensor(df, n_items, distribution_mode="steck")
 
 
 def build_tensors_from_df(df, weight_col):
+    """
+    Build tensors for users, items and weights from a pandas DataFrame.
+
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        DataFrame containing at least the columns named by the global constants USER_COL and ITEM_COL,
+        and the column specified by weight_col.
+    weight_col : str
+        Name of the weight column that determines the importance of interaction (user, item)
+
+    Returns
+    -------
+    x : torch.Tensor
+        1-D integer tensor of user identifiers/indices with shape (N,).
+    y : torch.Tensor
+        1-D integer tensor of item identifiers/indices with shape (N,).
+    weight : torch.Tensor
+        1-D float32 tensor of weights with shape (N,) on the device referenced by the global variable `dev`.
+
+    """
     if weight_col not in df.columns:
         raise ValueError(f"Column '{weight_col}' not found in DataFrame.")
     w_u_i_steck_df = df[[USER_COL, ITEM_COL, weight_col]]
@@ -58,10 +100,38 @@ def build_weight_tensor(
     item_tensor=None,
     ratings_tensor=None,
 ):
+    """
+    Create a dense user-item weight tensor.
+    ----------
+    Parameters
+
+    df : pandas.DataFrame
+        DataFrame used to construct tensors when user_tensor/item_tensor/ratings_tensor are None.
+    weight_col : str
+        Column name in df containing weight/rating values.
+    n_users : int
+        Number of users (first dimension of returned tensor).
+    n_items : int
+        Number of items (second dimension of returned tensor).
+    Optionals:
+    user_tensor : torch.LongTensor or array-like, optional
+        1D indices of users for each interaction (overrides df-based construction).
+    item_tensor : torch.LongTensor or array-like, optional
+        1D indices of items for each interaction (overrides df-based construction).
+    ratings_tensor : torch.Tensor or array-like, optional
+        1D weights/ratings for each interaction (overrides df-based construction).
+
+    Returns
+    -------
+    torch.FloatTensor of shape (n_users, n_items)
+        Dense tensor  with weights populated at (user, item)
+        locations; entries without interactions are zero.
+    """
     if user_tensor is None or item_tensor is None or ratings_tensor is None:
         user_tensor, item_tensor, ratings_tensor = build_tensors_from_df(df, weight_col)
     w_u_i_tensor = torch.zeros(size=(n_users, n_items), dtype=torch.float32, device=dev)
-    w_u_i_tensor[user_tensor, item_tensor] = ratings_tensor
+    indices = (user_tensor, item_tensor)
+    w_u_i_tensor.index_put_(indices, ratings_tensor, accumulate=True)
 
     return w_u_i_tensor
 
@@ -76,20 +146,6 @@ def build_user_genre_history_distribution(
 ):
     w_u_i_tensor = build_weight_tensor(df, weight_col, n_users=n_users, n_items=n_items)
     return (w_u_i_tensor @ p_g_i) / w_u_i_tensor.sum(dim=1, keepdim=True)
-
-
-def update_candidate_list_genre_distribution(
-    user, w_hat_u_i, item_distribution_tensor, candidate_list
-):
-    # In this specific case, w_hat_u_is just a vector, not a 2d matrix.
-    # So we sum on dim 0 instead of 1.
-    w_hat_norm = w_hat_u_i[user, candidate_list].sum(dim=0, keepdim=True)
-    subset_q_g_u = (
-        w_hat_u_i[user, candidate_list]
-        @ item_distribution_tensor[candidate_list]
-        / w_hat_norm
-    )
-    return torch.nan_to_num(subset_q_g_u, 0).reshape(-1)
 
 
 def clip_tensors_at_k(user_tensor, rec_ids, rec_scores, k):